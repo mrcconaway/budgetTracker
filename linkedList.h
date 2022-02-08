395c193456b817e599415db25572b956610a3cbb
mrcconaway <mrcconaway@gmail.com>
2022-02-08 15:50:22 -0500

    Getting ready to create a doubly linked list
    Skeleton sketch of doubly linked list.
    
    TODO:
    Add datafield data type (create appropriate class)
    Add implementation

--- linkedList.h
+++ linkedList.h
@@ -9,22 +9,7 @@ class linkedList
 public: 
     linkedList();
 public:
-    // list manipulation 
-    size_t list_length(const node* head_ptr);
-    void list_insert(node* previous_ptr, const node::value_type& entry);
-    void list_head_insert(node*& head_ptr, const node::value_type& entry);
-    void list_head_remove(node*& head_ptr);
-    void list_remove(node* previous_ptr);
-    void list_clear(node*& head_ptr);
-
-    // list accessors
-    node* list_search(node* head_ptr, const node::value_type& target);
-    const node* list_search(const node* head_ptr, const node::value_type& target);
-    node* list_locate(node* head_ptr, size_t position);
-    const node* list_locate(const node* head_ptr, size_t position);
-
-    // copy a list
-    void list_copy(const node* source_ptr, node*& head_ptr, node*& tail_ptr);
+    // implementation
 
 public: // The Big 3! 
     ~linkedList(); // Deconstructor
--- node.h
+++ node.h
@@ -1,31 +1,15 @@
 #pragma once
 
 #include <cstdlib>
+#include "data"
 
 class node
 {
 public:
-    // TYPEDEF
-    typedef double value_type;
-
-    node(const value_type& init_data = value_type(), node* init_link = nullptr)
-    {
-        data_field = init_data; next = init_link;
-    }
-
-    // members functions to set data and link field
-    void set_data(const value_type& new_data) { data_field = new_data; }
-    void set_link(node* new_link) { next = new_link; }
-
-    // constant member function to retrive current data
-    value_type data()const { return data_field; }
-
-    // two slightly different member functions to retreive current link
-    const node* link() const { return next; }
-    node* link() { return next; }
-
+    node();
 
 private:
-    value_type data_field;
+    data datafield;
     node* next;
+    node* prev;
 };
