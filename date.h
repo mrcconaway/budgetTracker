2b422e7990c2aea48d8db92baa01ca35e095f38a
mrcconaway <mrcconaway@gmail.com>
2022-02-08 21:19:25 -0500

    making big 3 for date.h

--- date.h
+++ date.h
@@ -3,8 +3,25 @@
 
 class date
 {
+public: // BIG 3! 
+    // constructor
     date(int y = 0, int d = 0, MONTH m = NONE){ year = y; day = d; month = m; }
+    
+    // copy constructor
+    date& date::operator =(const date& other){
+        if(this == &other){
+            return *this;
+        }
+    
+    // code of the destructor
+    // code of the copy constructor
 
+        return *this;
+    }
+
+
+
+public:
     // mutator
     void set_day(int d){ d = day; }
     void set_year(int y){ year = y; }
@@ -13,7 +30,8 @@ class date
     // accessor
     const int get_day(){ return day; }
     const int get_year(){ return year; }
-    const MONTH get_month(){ return m; }
+    const MONTH get_month(){ return month; }
+    
 
 
 private:
--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 
 int main()
 {
+    date test1;
+    test1.set_day(1);
+    test1.set_year(21);
+    test1.set_month(FEBRUARY);
+
+    date test2;
+    test1.set_day(2);
+    test1.set_year(22);
+    test1.set_month(JANUARY);
     
     return 1;
 }
