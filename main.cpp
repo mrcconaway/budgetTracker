de91d6eeedb35136c76a1af319e6c49d177b9b8b
mrcconaway <mrcconaway@gmail.com>
2022-02-10 13:07:26 -0500

    start of a node class (again)

--- main.cpp
+++ main.cpp
@@ -6,25 +6,7 @@
 int main()
 {
     
-    DATE d1(22, 5, APRIL);
-    BUDGET t1;
-    t1.set_amount(55.55);
-    t1.set_category(HOUSING);
-    t1.set_date(d1);
 
-    t1.output(std::cout);
-
-    BUDGET t2;
-    t2.output(std::cout);
-    t2 = t1;
-    t2.output(std::cout);
-
-    BUDGET t3;
-    d1.set_year(22);
-    t3.set_amount(200.00);
-    t3.set_category(GAS);
-    t3.set_date(d1);
-    t3.output(std::cout);
 
     return 1;
 }
---
+++ node.h
@@ -0,0 +1,17 @@
+
+#include "BUDGET.h"
+#include "date.h"
+#include "month.h"
+
+
+class node
+{
+public:
+
+
+
+private:
+    BUDGET budget;  
+    node* next;
+    node* prev;
+};
