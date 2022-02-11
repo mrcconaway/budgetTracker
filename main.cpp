0edb30065a8fac19a04c44d864c069387ad71986
mrcconaway <mrcconaway@gmail.com>
2022-02-10 20:50:24 -0500

    cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -8,23 +8,7 @@
 
 int main()
 {
-    DATE d1;
-    DATE d2(12,0,APRIL);
-    DATE d3(7,35,NOVEMBER);
 
-    BUDGET b1(88, d1, UNKNOWN);
-    BUDGET b2(55, d2, GAS);
-    BUDGET b3 = b1;
-
-    list l;
-    l.add_item(b1);
-    l.add_item(b2);
-    l.add_item(b3);
-    l.display();
-
-    std::ofstream fout;
-    fout.open("test1.txt");
-    l.display(fout);
 
 
 
