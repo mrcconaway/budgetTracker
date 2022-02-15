9a998ae3427cf026fd259aec634a4d94ed6cec0f
mrcconaway <mrcconaway@gmail.com>
2022-02-15 12:23:59 -0500

    got save() working and refactored parse_dir_and_file so that it no longer returns anything
    changed name of parse_dir_and_file to better reflect that it does not return a file name anymore

--- main.cpp
+++ main.cpp
@@ -5,21 +5,19 @@
 #include <filesystem>
 
 using namespace std;
-namespace fs = filesystem;
 
 
-// void save()
-// {
-//     std::ofstream fout;
-//     fout.open(saveFile.c_str());
-//     if(fout.fail()){
-//         std::cout << "ERROR";
-//     }
-//     else{
-//         fout << "TEST TEST TEST TEST" << std::endl;
-//         fout.close();
-//     }
-// }
+void save(std::ofstream& fout)
+{
+
+    if(fout.fail()){
+        std::cout << "ERROR";
+    }
+    else{
+        fout << "TEST TEST TEST TEST" << std::endl;
+        fout.close();
+    }
+}
 
 
 void make_dir(std::vector<std::string> vinp)
@@ -29,16 +27,16 @@ void make_dir(std::vector<std::string> vinp)
         dirTree += vinp[i];
         dirTree += "/";
     }  
-    fs::create_directories(dirTree.c_str());
+    std::filesystem::create_directories(dirTree.c_str());
 
 }
 
 // given and a directory path and file name (ie dir1/dir2/..), it parses the directories and returns the file name
 // Note: No directories should be given in the form /dir1/dir2/... so my logic could be reworked (the logic rn is designed so that
 // the exception is the rule)
-string parse_Dir_and_File(std::string dirInp){
-    string tmp;
-    vector<string> dir;
+void parse_Dir(std::string dirInp){
+    std::string tmp;
+    std::vector<string> dir;
     int count = 0;
     if(dirInp[0] != '/' || dirInp[0] != '/'){ // in case a path is given in the form dir0/dir1/file.txt
                                                   // converts it to the equivalent /dir0/dir1/file.txt so the logic works correctly
@@ -59,22 +57,18 @@ string parse_Dir_and_File(std::string dirInp){
     }
     if(dir.size() != 0)
         make_dir(dir); // create directory tree
-    return tmp;
+    return;
 }
 
 int main()
 {
-    string saveFile = "TEST/TEST/test.txt";
+    std::string saveFile = "TEST/TEST/test.txt";
 
-    string file = parse_Dir_and_File(saveFile);
-    // save();
+    parse_Dir(saveFile);
 
     std::ofstream fout;
     fout.open(saveFile.c_str());
-
-    fout << "help";
-    fout.close();
-
+    save(fout);
 
     return 1;
 }
