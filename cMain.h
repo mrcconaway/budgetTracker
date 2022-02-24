05dece89e76aac94aff8380f7f47dc016a66a2ae
mrcconaway <mrcconaway@gmail.com>
2022-02-24 13:34:41 -0500

    Basic Button and Event Handling

--- cMain.h
+++ cMain.h
@@ -6,6 +6,13 @@ class cMain : public wxFrame
 public:
     cMain();
     ~cMain();
+public:
+    wxButton* m_btn1 = nullptr;
+    wxTextCtrl* m_txt1 = nullptr;
+    wxListBox* m_list1 = nullptr;
 
+    void OnButtonClicked(wxCommandEvent &evt);
+
+    wxDECLARE_EVENT_TABLE();
 
 };
